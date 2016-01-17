edfe592b4768e322093cf8197c49200733287aff
imaricodes <imaricodes@gmail.com>
2016-01-16 22:47:44 -0500

    first commit

---
+++ codeEval-AgeDistribution-20160115/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codeEval_AgeDistribution_20160115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
